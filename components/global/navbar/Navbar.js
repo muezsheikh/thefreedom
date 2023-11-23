723903fef21b6e1e6f458e77ee17d13dab60b1d6
MuezSheikh <muezbusiness@gmail.com>
2023-11-23 16:56:59 +0500

    some more changes

--- components/global/navbar/Navbar.js
+++ components/global/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import NavLinks from './Nav-links';
 import MobNavLinks from './mob-nav-Links';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
@@ -11,9 +12,11 @@ export default function Navbar() {
         <div className="date">
           <p>Tuesday, November 21, 2023</p>
         </div>
-        <div className='mainLogo'>
-          <img src="https://thefreedom.com.pk/wp-content/uploads/2021/10/The-freedom-logoo.png" alt="" />
-        </div>
+        <Link href={"/"}>
+          <div className='mainLogo'>
+            <img src="https://thefreedom.com.pk/wp-content/uploads/2021/10/The-freedom-logoo.png" alt="" />
+          </div>
+        </Link>
         {/* <img src="/images/logo.png" alt="" /> */}
       </div>
       <div className='largeScreen'>
--- components/global/navbar/mob-nav-Links.js
+++ components/global/navbar/mob-nav-Links.js
@@ -19,50 +19,50 @@ export default function MobNavLinks() {
 
   return (
     <>
-    <div className="navActiveIcon" onClick={activeNavFunc}>
-      <i className="fas fa-bars"></i>
-    </div>
-    <div className={`mobNav ${activeNav ? 'activeN' : ''}`}>
-      <div className="crossIcon" onClick={activeNavFunc}>
-        <div className="iconContent">
-          <i className="fas fa-xmark"></i>
-        </div>
+      <div className="navActiveIcon" onClick={activeNavFunc}>
+        <i className="fas fa-bars"></i>
       </div>
-      <ul>
-        <li><Link href={'/'}>Home</Link></li>
-        <li onClick={activeDrop1Func}>
-          <div className="dropLink">
-            <Link href={'/'}>World</Link>
-            <div className="linkIcon">
-              <i className={`fas fa-arrow-${activeDrop1 ? 'up': 'down'}`}></i>
-            </div>
+      <div className={`mobNav ${activeNav ? 'activeN' : ''}`}>
+        <div className="crossIcon" onClick={activeNavFunc}>
+          <div className="iconContent">
+            <i className="fas fa-xmark"></i>
           </div>
-          <ul className={`mobDropdown ${activeDrop1 ? 'activeD1' : ''}`}>
-            <li><Link href={'/south-asia'}>South Asia</Link></li>
-            <li><Link href={'/pakistan'}>Pakistan</Link></li>
-            <li><Link href={'/middle-east'}>Middle East</Link></li>
-          </ul>
-        </li>
-        <li onClick={activeDropFunc}>
-          <div className='dropLink'>
-            <Link href={'/'}>Business</Link>
-            <div className="linkIcon">
-              <i className={`fas fa-arrow-${activeDrop ? 'up': 'down'}`}></i>
+        </div>
+        <ul>
+          <li onClick={() => setActiveNav(false)}><Link href={'/'}>Home</Link></li>
+          <li onClick={activeDrop1Func}>
+            <div className="dropLink">
+              <Link href={'/'}>World</Link>
+              <div className="linkIcon">
+                <i className={`fas fa-arrow-${activeDrop1 ? 'up' : 'down'}`}></i>
+              </div>
             </div>
-          </div>
-          <ul className={`mobDropdown ${activeDrop ? 'activeD' : ''}`}>
-            <li><Link href={'/crypto'}>Crypto</Link></li>
-          </ul>
-        </li>
-        <li><Link href={'/environment'}>Environment</Link></li>
-        <li><Link href={'/health'}>Health</Link></li>
-        <li><Link href={'/education'}>Education</Link></li>
-        <li><Link href={'/visa-and-immigration'}>Visa and Immigration</Link></li>
-        <li><Link href={'/fashion-and-style'}>Fashion & Style</Link></li>
-        <li><Link href={'/sports'}>Sports</Link></li>
-        <li><Link href={'/contact'}>Contact</Link></li>
-      </ul>
-    </div>
+            <ul className={`mobDropdown ${activeDrop1 ? 'activeD1' : ''}`}>
+              <li onClick={() => setActiveNav(false)}><Link href={'/south-asia'}>South Asia</Link></li>
+              <li onClick={() => setActiveNav(false)}><Link href={'/pakistan'}>Pakistan</Link></li>
+              <li onClick={() => setActiveNav(false)}><Link href={'/middle-east'}>Middle East</Link></li>
+            </ul>
+          </li>
+          <li onClick={activeDropFunc}>
+            <div className='dropLink'>
+              <Link href={'/'}>Business</Link>
+              <div className="linkIcon">
+                <i className={`fas fa-arrow-${activeDrop ? 'up' : 'down'}`}></i>
+              </div>
+            </div>
+            <ul className={`mobDropdown ${activeDrop ? 'activeD' : ''}`}>
+              <li onClick={() => setActiveNav(false)}><Link href={'/crypto'}>Crypto</Link></li>
+            </ul>
+          </li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/environment'}>Environment</Link></li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/health'}>Health</Link></li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/education'}>Education</Link></li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/visa-and-immigration'}>Visa and Immigration</Link></li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/fashion-and-style'}>Fashion & Style</Link></li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/sports'}>Sports</Link></li>
+          <li onClick={() => setActiveNav(false)}><Link href={'/contact'}>Contact</Link></li>
+        </ul>
+      </div>
     </>
   )
 }
