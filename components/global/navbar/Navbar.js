6c030d2d9cad465bcf807c795935753808272eb1
Your Name <muezpersonal@outlook.com>
2023-11-24 19:50:38 +0500

    search bar

--- components/global/navbar/Navbar.js
+++ components/global/navbar/Navbar.js
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
-import NavLinks from './Nav-links';
-import MobNavLinks from './mob-nav-Links';
-import Link from 'next/link';
+import NavLinks from './Nav-links'
+import MobNavLinks from './mob-nav-Links'
+import Link from 'next/link'
 
 export default function Navbar() {
-   const formatDate = (date) => {
-    const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
-    return new Date(date).toLocaleDateString('en-US', options);
-  };
+  const [activeSearch, setActiveSearch] = useState(false)
+  const activeSearchFunc = () => {
+    setActiveSearch(!activeSearch)
+  }
+  const formatDate = (date) => {
+    const options = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    }
+    return new Date(date).toLocaleDateString('en-US', options)
+  }
 
-  const currentDate = formatDate(new Date());
+  const currentDate = formatDate(new Date())
 
   return (
     <>
       <Header />
-      <div className="main-logo">
-        <div className="date">
+      <div className='main-logo'>
+        <div className='date'>
           <p>{currentDate}</p>
         </div>
-        <Link href={"/"}>
+        <Link href={'/'}>
           <div className='mainLogo'>
-            <img src="https://thefreedom.com.pk/wp-content/uploads/2021/10/The-freedom-logoo.png" alt="" />
+            <img
+              src='https://thefreedom.com.pk/wp-content/uploads/2021/10/The-freedom-logoo.png'
+              alt=''
+            />
           </div>
         </Link>
-        {/* <img src="/images/logo.png" alt="" /> */}
+        <div className='searchBar' onClick={activeSearchFunc}>
+          <i className='fas fa-search'></i>
+        </div>
+
+        <div className={`searchBarContainer ${activeSearch ? "activeSI" : ""}`}>
+          <input type='text' placeholder='Search...' />
+          <div className='searchClickIcon' onClick={activeSearchFunc}>
+            <i className='fas fa-search'></i>
+          </div>
+        </div>
       </div>
       <div className='largeScreen'>
         <NavLinks />
       </div>
-      <div className="mobScreen">
+      <div className='mobScreen'>
         <MobNavLinks />
       </div>
-
     </>
   )
 }
